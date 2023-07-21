b2d47c1a1b68aa6f92dbc0074f0bf5fb5ee71fec
shiv_2001_tava <104251433+shiv2001tava@users.noreply.github.com>
2023-07-21 16:40:54 +0530

    Create majority1.cpp

---
+++ majority1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int majorityElement(vector<int> v) {
+    int n = v.size();
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+    }
+
+    for (auto it : mpp) {
+        if (it.second > (n / 2)) {
+            return it.first;
+        }
+    }
+    return -1;
+}
+
