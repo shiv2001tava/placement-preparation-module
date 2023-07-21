9d16e73fb67a3fad67a144e12a6465f68b004684
shiv_2001_tava <104251433+shiv2001tava@users.noreply.github.com>
2023-07-21 16:39:43 +0530

    Create power.cpp

---
+++ power.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+double solution(double x, int n) {
+      double ans = 1.0;
+      for (int i = 0; i < n; i++) {
+        ans = ans * x;
+      }
+      return ans;
+}
